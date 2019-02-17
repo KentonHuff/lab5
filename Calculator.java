758a8e65e8a483483e8b977bd4a723e4ad9a3867
Kenton Huff <kentonahuff@gmail.com>
2019-02-17 16:07:34 -0600

    Implemented execute method

--- Calculator.java
+++ Calculator.java
@@ -143,20 +143,15 @@ public class Calculator
         // Condition on the number of tokens (number of strings in user input separated by spaces)
         switch(tokens.length)
         {
-        case 0: throw new CalculatorException("Illegal Token Length");
-        	//break;
-        case 1: 
-        	if(tokens[0].equals("quit"))
-        		return Integer.MIN_VALUE;
-        	else
-        		throw new CalculatorException("Illegal Command");
-        	//break;
-        case 2: return calculateTwoTokens(tokens);
-        	//break;
-        case 3: return calculateThreeTokens(tokens);
-        	//break;
-        default: throw new CalculatorException("Illegal Token Length");
-        	//break;
+	        case 0: throw new CalculatorException("Illegal Token Length");
+	        case 1: 
+	        	if(tokens[0].equals("quit"))
+	        		return Integer.MIN_VALUE;
+	        	else
+	        		throw new CalculatorException("Illegal Command");
+	        case 2: return calculateTwoTokens(tokens);
+	        case 3: return calculateThreeTokens(tokens);
+	        default: throw new CalculatorException("Illegal Token Length");
         }
 
     }
